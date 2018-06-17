e59c3fc360f7618ffa522b225c43098a9ebd920e
Balakrishna259 <s528771@mail.nwmissouri.edu>
2018-06-17 10:51:56 -0500

    Excel and Exception added

---
+++ ExcelNotFoundException.java
@@ -0,0 +1,10 @@
+package com.java.excel;
+
+public class ExcelNotFoundException extends Exception {
+
+	public ExcelNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+	
+	
+}
